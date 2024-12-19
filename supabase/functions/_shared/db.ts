afe71c717f33967b57e7d0fe4dd080b843bffd51
Matej Lauko <dev@lauko.io>
2024-12-19 21:47:17 +0100

    deploy fix

--- supabase/functions/_shared/db.ts
+++ supabase/functions/_shared/db.ts
@@ -2,10 +2,10 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from '../_database/schema.ts'
 
-const connectionString = Deno.env.get('SUPABASE_DB_URL')
+const connectionString = Deno.env.get('DB_URL')
 
 if (!connectionString) {
-  throw new Error('SUPABASE_DB_URL is not set')
+  throw new Error('DB_URL is not set')
 }
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
